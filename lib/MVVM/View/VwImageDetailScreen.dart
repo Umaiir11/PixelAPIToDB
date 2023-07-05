ac4c9f8de4168974be287b46d77d5141d19c7159
Umair11 <iam.umairimran@gmail.com>
2023-07-05 10:56:51 +0500

    images fetched

--- lib/MVVM/View/VwImageDetailScreen.dart
+++ lib/MVVM/View/VwImageDetailScreen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'VmImageDetailsScreen.dart';
+
 
 class ImageDetailScreen extends StatelessWidget {
   final ImageProvider image;
-
-  const ImageDetailScreen({required this.image});
+  final  l_VmImageDetailScreen = Get.put(VmImageDetailController());
+  ImageDetailScreen({required this.image});
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +15,32 @@ class ImageDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Image'),
       ),
-      body: Center(
-        child: Hero(
-          tag: image, // Use the same unique tag for the Hero widget
-          child: Image(
-            image: image,
-            fit: BoxFit.contain,
-
+      body: GestureDetector(
+        onDoubleTap: () {
+          if (l_VmImageDetailScreen.scale.value == 1.0) {
+            l_VmImageDetailScreen.scale.value = 2.0;
+          } else {
+            l_VmImageDetailScreen.scale.value = 1.0;
+          }
+        },
+        child: Center(
+          child: GestureDetector(
+            onScaleUpdate: (ScaleUpdateDetails details) {
+              l_VmImageDetailScreen.scale.value = l_VmImageDetailScreen.initialScale.value * details.scale;
+            },
+            onScaleEnd: (ScaleEndDetails details) {
+              l_VmImageDetailScreen.initialScale.value = l_VmImageDetailScreen.scale.value;
+            },
+            child: Hero(
+              tag: image,
+              child: Obx(() => Transform.scale(
+                scale: l_VmImageDetailScreen.scale.value,
+                child: Image(
+                  image: image,
+                  fit: BoxFit.contain,
+                ),
+              )),
+            ),
           ),
         ),
       ),
