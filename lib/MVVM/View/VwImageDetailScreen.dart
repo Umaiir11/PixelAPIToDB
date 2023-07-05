1c02dfe53ae66bf89d8fa2fb5ebfc29e0ef5a7d7
Umair11 <iam.umairimran@gmail.com>
2023-07-05 15:10:35 +0500

    images fetched

--- lib/MVVM/View/VwImageDetailScreen.dart
+++ lib/MVVM/View/VwImageDetailScreen.dart
@@ -33,7 +33,7 @@ class VwImageDetailScreen extends StatelessWidget {
               l_VmImageDetailController.scale.value = l_VmImageDetailController.initialScale.value * details.scale;
             },
             onScaleEnd: (ScaleEndDetails details) {
-              l_VmImageDetailController.initialScale.value = l_VmImageDetailController.scale.value;
+              l_VmImageDetailController.initialScale.value = 1.0;
             },
             child: Hero(
               tag: image,
